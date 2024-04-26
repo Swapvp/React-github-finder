fc0ab561e9f3ecef3a216fea3fd77b7d2a5d5b16
Swapvp <swapneshpawar1994@gmail.com>
2024-04-26 13:08:09 +0530

    github finder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import GithubProfileFinder from "./Components/GithubProfileFinder";
+
+function App() {
+  return (
+    <>
+      <GithubProfileFinder></GithubProfileFinder>
+    </>
+  );
+}
+
+export default App;
